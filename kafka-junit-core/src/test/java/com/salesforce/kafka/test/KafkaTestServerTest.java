c4fa62f7f7d8b27754f6320528cbc5decc163326
Stephen Powis <spowis@salesforce.com>
2018-04-24 15:59:00 +0900

    Code cleanup, fix style violations

--- kafka-junit-core/src/test/java/com/salesforce/kafka/test/KafkaTestServerTest.java
+++ kafka-junit-core/src/test/java/com/salesforce/kafka/test/KafkaTestServerTest.java
@@ -1,3 +1,28 @@
+/**
+ * Copyright (c) 2017-2018, Salesforce.com, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ *   disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Salesforce.com nor the names of its contributors may be used to endorse or promote products
+ *   derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package com.salesforce.kafka.test;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
@@ -5,12 +30,13 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.Properties;
 
 /**
@@ -32,25 +58,23 @@ public class KafkaTestServerTest {
             kafkaTestServer.start();
             kafkaTestServer.createTopic(theTopic, 1);
 
+            // Define override properties.
             Properties config = new Properties();
-            config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaTestServer.getKafkaConnectString());
             config.put("group.id", "test-consumer-group");
             config.put("enable.auto.commit", "false");
             config.put(ConsumerConfig.ISOLATION_LEVEL_CONFIG, "read_committed");
             config.put("auto.offset.reset", "earliest");
-            config.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-            config.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 
-            try (final KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(config)) {
-                consumer.subscribe(Arrays.asList(theTopic));
+            try (final KafkaConsumer<String, String> consumer
+                     = kafkaTestServer.getKafkaConsumer(StringDeserializer.class, StringDeserializer.class, config)) {
+                consumer.subscribe(Collections.singletonList(theTopic));
 
-                //setup the producer
+                // Setup the producer
                 config = new Properties();
-                config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaTestServer.getKafkaConnectString());
                 config.put("transactional.id", "MyRandomString" + System.currentTimeMillis());
-                config.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-                config.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-                try (final KafkaProducer<String, String> producer = new KafkaProducer<>(config)) {
+
+                try (final KafkaProducer<String, String> producer
+                         = kafkaTestServer.getKafkaProducer(StringSerializer.class, StringSerializer.class, config)) {
                     // Init transaction and begin
                     producer.initTransactions();
                     producer.beginTransaction();
