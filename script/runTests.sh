e7a9d2b8898fd09222341d95554e4536423ff0da
Stephen Powis <spowis@salesforce.com>
2022-03-01 13:38:53 +0900

    bump minor release versions in docs to most recent

--- script/runTests.sh
+++ script/runTests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ## Adjust values to suite requirements.
-export KAFKA_VERSION=2.7.1
-export KAFKA_SCALA_VERSION=kafka_2.13
+export KAFKA_VERSION=2.8.1
+export KAFKA_SCALA_VERSION=kafka_2.12
 export EXCLUDE_KAFKA_TESTS=0_11_0_x
 
 mvn clean test -B -DkafkaVersion=$KAFKA_VERSION -DkafkaScalaVersion=$KAFKA_SCALA_VERSION -Dtests.excluded=$EXCLUDE_KAFKA_TESTS -DskipCheckStyle=true -Djava.security.auth.login.config=${PWD}/kafka-junit-core/src/test/resources/jaas.conf
